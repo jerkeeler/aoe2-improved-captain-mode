303b323ff52f654fde2eb1177c34873fb7a1c9d8
Jeremy Keeler <jerkeeler@gmail.com>
2020-04-12 10:46:32 -0400

    Add new logos and clear state

--- frontend/src/pages/draft/Draft/Draft.tsx
+++ frontend/src/pages/draft/Draft/Draft.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { DraftState, Role } from '@icm/shared/types';
 
 import useDraftSocket from '../../../hooks/useDraftSocket';
 import useDraftState from '../../../hooks/useDraftState';
 import DraftMinimap from '../../../components/DraftMinimap';
+import { clearState } from '../../../store/draftSlice';
 import CaptainPicks from '../CaptainPicks';
 import DraftStatus from '../DraftStatus';
 import DraftLink from '../DraftLink';
@@ -11,6 +13,13 @@ import styles from './Draft.module.css';
 import CivPicker from '../CivPicker';
 
 const Draft = () => {
+  const dispatch = useDispatch();
+  useEffect(
+    () => () => {
+      dispatch(clearState());
+    },
+    [dispatch],
+  );
   useDraftSocket();
   const { draftInfo, draftConfig, role } = useDraftState();
   const activeIdx = draftInfo.state === DraftState.FINISHED ? -1 : draftInfo.currentActionIdx;
